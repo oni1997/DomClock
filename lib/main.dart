9d22a323ae1f147bebf366d8626cd39fe4728131
onmaenz022 <onmaenz022@student.wethinkcode.co.za>
2023-06-15 14:37:01 +0200

    alarm

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(DomClockApp());
 }
@@ -9,6 +10,7 @@ class DomClockApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Dom Clock',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -26,6 +28,8 @@ class DomClockScreen extends StatefulWidget {
 class _DomClockScreenState extends State<DomClockScreen> {
   Timer? _timer;
   String _currentTime = '';
+  String _alarmTime = '';
+  int _selectedIndex = 0;
 
   @override
   void initState() {
@@ -42,8 +46,14 @@ class _DomClockScreenState extends State<DomClockScreen> {
   void _startTimer() {
     _timer = Timer.periodic(Duration(seconds: 1), (_) {
       setState(() {
+        DateTime now = DateTime.now();
         _currentTime =
-            '${DateTime.now().hour.toString().padLeft(2, '0')}:${DateTime.now().minute.toString().padLeft(2, '0')}:${DateTime.now().second.toString().padLeft(2, '0')}';
+            '${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}:${now.second.toString().padLeft(2, '0')}';
+
+        if (_alarmTime.isNotEmpty && _alarmTime == _currentTime) {
+          _showAlarmDialog();
+          _stopTimer();
+        }
       });
     });
   }
@@ -53,27 +63,153 @@ class _DomClockScreenState extends State<DomClockScreen> {
     _timer = null;
   }
 
+  Future<void> _showAlarmDialog() async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Alarm'),
+          content: Text('Time to wake up!'),
+          actions: [
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _setAlarmTime() async {
+    TimeOfDay? selectedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+
+    if (selectedTime != null) {
+      setState(() {
+        _alarmTime =
+            '${selectedTime.hour.toString().padLeft(2, '0')}:${selectedTime.minute.toString().padLeft(2, '0')}:00';
+      });
+    }
+  }
+
+  void _cancelAlarm() {
+    setState(() {
+      _alarmTime = '';
+    });
+  }
+
+  void _onNavItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _buildClockScreen() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Current Time',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          Text(
+            _currentTime,
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAlarmScreen() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Alarm',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          Text(
+            _alarmTime.isNotEmpty ? '$_alarmTime' : 'No alarm set',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 40),
+          TextButton(
+            child: Text(
+              _alarmTime.isNotEmpty ? 'Cancel Alarm' : 'Set Alarm',
+              style: TextStyle(fontSize: 18),
+            ),
+            onPressed: () {
+              if (_alarmTime.isNotEmpty) {
+                _cancelAlarm();
+              } else {
+                _setAlarmTime();
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTimerScreen() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Timer',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Work in progress',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> _screens = [
+      _buildClockScreen(),
+      _buildAlarmScreen(),
+      _buildTimerScreen(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Dom Clock'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'The Time Now',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            Text(
-              _currentTime,
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onNavItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: 'Clock',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: 'Alarm',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer),
+            label: 'Timer',
+          ),
+        ],
       ),
     );
   }
